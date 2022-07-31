2d20f1d3f3e3d9b570050158762c4c30381650bb
David Emiliano Morales <105241040+DEMAcomputacion@users.noreply.github.com>
2022-07-31 18:13:35 -0300

    fetch fotos y respuesta buscador
    so cambia la forma en que se usa la llamada de fotos y se grega funcionalidad de mensajes de error del buscador

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import * as ui from './ui.js';
 import * as llamadas from './llamadas.js';
 
+//previene que se recargue la pagina al presionar enter
+document.querySelector("#nombreONumeroPokemon")
+    .addEventListener("keydown", (evento) => {
+        if (evento.key == "Enter") {
+            evento.preventDefault();
+            return false;
+            }
+        }
+    );
 
 async function inicializa() {
-    const listado = await llamadas.llamaListadoPokemones(0)
-    ui.muestraListadoPokemones(listado);
-    ui.mustraPaginasDisponibles(listado);
+    const listadoDePokemones = await llamadas.llamaListadoPokemones(0)
+    ui.muestraListadoPokemones(listadoDePokemones);
+    ui.mustraPaginasDisponibles(listadoDePokemones);
     ui.muestraPokemon();
+    ui.cargaFuncionesBotonesPaginas(listadoDePokemones, 0)
 }
 
-export async function actualizaListado(nroPagina, offset) {
-    const listado = await llamadas.llamaListadoPokemones(offset.desde, 20)
-    ui.muestraListadoPokemones(listado, nroPagina);
-    ui.mustraPaginasDisponibles(listado, nroPagina);
-    ui.muestraPokemon();
+export async function actualizaListado(nroPagina, desde) {
+    const listadoDePokemones = await llamadas.llamaListadoPokemones(desde, 20)
+    ui.muestraListadoPokemones(listadoDePokemones, nroPagina);
+    ui.mustraPaginasDisponibles(listadoDePokemones, nroPagina);
+    ui.muestraPokemon(desde + 1);
+    ui.cargaFuncionesBotonesPaginas(listadoDePokemones, nroPagina)
 }
 
 
